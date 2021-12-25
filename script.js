86003441b9a577a3f930bd8f383e82716ce8d0eb
jsncorn <jsncourn@gmail.com>
2021-12-25 06:24:52 -0800

    finish if logic

--- script.js
+++ script.js
@@ -23,18 +23,46 @@ generateBtn.addEventListener("click", writePassword);
 //numeric, or special characters
 
 function generatePassword() {
-promptUser();
-checkCriteria();
+  promptUser();
+  checkCriteria();
+  }
 }
 
 function checkCriteria() {
-  if(valueLowercase && valueUppercase && valueNumbers && valueSpecial) {
-  const finalArray = finalArray.concat(lowArray, upperArray, numArray, specArray);
+  if(!valueLowercase && !valueUppercase && !valueNumbers && !valueSpecial){
+    alert("Invalid password criteria, please select at least one criteria.");
+    promptUser();
   }
-  else if(!valueLowercase && valueUppercase && valueNumbers && valueSpecial){
-  const finalArray = lowArray.concat(upperArray, numArray, specArray);
+  else if (valueLowercase && valueUppercase && valueNumbers && valueSpecial) {
+    const finalArray = finalArray.concat(lowArray, upperArray, numArray, specArray);
   }
-
+  else if (valueLowercase && valueUppercase && valueNumbers) {
+    const finalArray = finalArray.concat(lowArray, upperArray, numArray);
+  }
+  else if (valueLowercase && valueUppercase && valueSpecial) {
+    const finalArray = finalArray.concat(lowArray, upperArray, specArray);
+  }
+  else if (valueUppercase && valueNumbers && valueSpecial) {
+    const finalArray = finalArray.concat(upperArray, numArray, specArray);
+  }
+  else if (valueLowercase && valueUppercase) {
+    const finalArray = finalArray.concat(lowArray, upperArray);
+  }
+  else if (valueLowercase && valueNumbers) {
+    const finalArray = finalArray.concat(lowArray, numArray);
+  }
+  else if (valueLowercase && valueSpecial) {
+    const finalArray = finalArray.concat(lowArray, specArray);
+  }
+  else if (valueUppercase && valueNumbers) {
+    const finalArray = finalArray.concat(upperArray, numArray);
+  }
+  else if (valueUppercase && valueSpecial) {
+    const finalArray = finalArray.concat(upperArray, specArray);
+  }
+  else if (valueNumbers && valueSpecial) {
+    const finalArray = finalArray.concat(numArray, specArray);
+  }
 }
 
 function promptUser() {
