a7dec0bab90068d19e29dd58791eaba2ab29eab9
jsncorn <jsncourn@gmail.com>
2021-12-25 05:36:00 -0800

    added functions for each pwd criteria

--- script.js
+++ script.js
@@ -5,10 +5,36 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//on click, prompt for: length (8-128), inc lowercase, uppercase
+//numeric, or special characters
+
+function generatePassword() {
+
+}
+
+function pwdLength() {
+  prompt("Password length")
+}
+
+function pwdLowercase() {
+  confirm("Lowercase?")
+}
+
+function pwdUppercase() {
+  confirm("Uppercase?")
+}
+
+function pwdNumeric() {
+  confirm("Numbers?")
+}
+
+function pwdSpecial() {
+  confirm("Special chars?")
+}
