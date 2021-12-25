4da807afbd007f636718d1662a4a1dde7f362cab
jsncorn <jsncourn@gmail.com>
2021-12-25 06:52:12 -0800

    added comments

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ var generateBtn = document.querySelector("#generate");
 
 //array for each criteria
 var lowArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var specArray = ['!@#$%^&*()'];
+
+//array for passwords
 var finalArray = [];
 var passwordRaw = [];
 
@@ -24,23 +26,31 @@ generateBtn.addEventListener("click", writePassword);
 //numeric, or special characters
 
 function generatePassword() {
+  //includes all prompts for criterias and takes values
   promptUser();
+  //ensures what criterias are true and runs absed off that
   checkCriteria();
+  //for loop to RNG the password based on final built array based off criteria
   for (var i = 0; i < valueLength; i++) {
-  var criteriaChoice = finalArray[Math.floor(Math.random() * finalArray.length)]
-  passwordRaw.push(criteriaChoice);
-  password = passwordRaw.join('');
-}
+    var criteriaChoice = finalArray[Math.floor(Math.random() * finalArray.length)]
+    //.push will add a new element to array passwordRaw
+    passwordRaw.push(criteriaChoice);
+    //.join will combine all element in passwordRaw and remove the commas
+    password = passwordRaw.join('');
+  }
 }
 
 function checkCriteria() {
+  //checks if all is false
   if (!valueLowercase && !valueUppercase && !valueNumbers && !valueSpecial) {
     alert("Invalid password criteria, please select at least one criteria.");
     promptUser();
   }
+  //checks if all is true
   else if (valueLowercase && valueUppercase && valueNumbers && valueSpecial) {
     finalArray = finalArray.concat(lowArray, upperArray, numArray, specArray);
   }
+  //triple checks
   else if (valueLowercase && valueUppercase && valueNumbers) {
     finalArray = finalArray.concat(lowArray, upperArray, numArray);
   }
@@ -50,6 +60,7 @@ function checkCriteria() {
   else if (valueUppercase && valueNumbers && valueSpecial) {
     finalArray = finalArray.concat(upperArray, numArray, specArray);
   }
+  //double checks
   else if (valueLowercase && valueUppercase) {
     finalArray = finalArray.concat(lowArray, upperArray);
   }
@@ -68,6 +79,7 @@ function checkCriteria() {
   else if (valueNumbers && valueSpecial) {
     finalArray = finalArray.concat(numArray, specArray);
   }
+  //single checks
   else if (valueLowercase) {
     finalArray = lowArray;
   }
@@ -80,11 +92,13 @@ function checkCriteria() {
   else if (valueSpecial) {
     finalArray = specArray;
   }
+  //error log :)
   else {
     console.log("you did something terribly wrong")
   }
 }
 
+//helps separate the prompts and give them indiviual values
 function promptUser() {
   pwdLength();
   pwdLowercase();
