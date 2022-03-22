a04b61c9c51160ca330dd58b013a0c863f6354b9
jsncorn <jsncourn@gmail.com>
2022-03-22 13:25:25 -0700

    fixed the ! being a 1 in special numbers

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 var lowArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var specArray = ["1", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var specArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 //array for passwords
 var finalArray = [];
